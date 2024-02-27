d644705f0145c67113578f5221d469856275defb
Oyuco <marcos132625@gmail.com>
2024-02-27 13:42:06 -0400

    Push Deploy

--- client/src/URL.js
+++ client/src/URL.js
@@ -1,11 +1,11 @@
-// export const URL_ACTIVITIES = 'https://cr-pi-countries-main.onrender.com/activities';
-// export const URL_COUNTRIES ="https://cr-pi-countries-main.onrender.com/countries"
-// export const URL_COUNTRIES_NAME = "https://cr-pi-countries-main.onrender.com/countries/name/?name=";
+export const URL_ACTIVITIES = 'https://cr-pi-countries-main.onrender.com/activities';
+export const URL_COUNTRIES ="https://cr-pi-countries-main.onrender.com/countries"
+export const URL_COUNTRIES_NAME = "https://cr-pi-countries-main.onrender.com/countries/name/?name=";
 
 //LOCALHOST
 
-export const URL_COUNTRIES_NAME = "http://localhost:3001/countries/name/?name=";
-export const URL_COUNTRIES= "http://localhost:3001/countries" 
-export const URL_ACTIVITIES= "http://localhost:3001/activities" 
+// export const URL_COUNTRIES_NAME = "http://localhost:3001/countries/name/?name=";
+// export const URL_COUNTRIES= "http://localhost:3001/countries" 
+// export const URL_ACTIVITIES= "http://localhost:3001/activities" 
 
 
